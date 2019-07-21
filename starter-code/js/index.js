dccdbc2ed95f06b17b799db70c7b148373e5e9c3
Inma <inmanewmail@gmail.com>
2019-07-21 11:10:58 +0200

    5º iteración pero con fallos

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,114 +1,103 @@
 
-// function deleteItem(e){
 
-// }
+let calculate_button = document.getElementById("calculateButton")  
 
+let product_container = document.getElementsByClassName('productContainer')
+// con esto saco un array que tiene por elementos los divs de clase productContainer
+// que contiene cada uno a cada elemento en el html
 
-// function getPriceByProduct(itemNode){
+let calcTotalPrice = 0 // la inicializo aquí por el scope y la uso luego fuera del loop
 
-// ------------------------PRODUCT ONE-------------------------
-
-
-
-  let user_input_product1= document.getElementsByClassName("userInput product1")[0]
-  let product_cost_product1 = document.getElementsByClassName("productCost product1")[0]
-  let multi_product_cost_product1 = document.getElementsByClassName("multiProductCost product1")[0]
+calculate_button.onclick = () => {
   
-// -------------------------PRODUCT TWO ---------------------------------
-
-
-  let user_input_product2= document.getElementsByClassName("userInput product2")[0]
-  let product_cost_product2 = document.getElementsByClassName("productCost product2")[0]
-  let multi_product_cost_product2 = document.getElementsByClassName("multiProductCost product2")[0]
- // let multi_product_cost = document.getElementsByClassName(".multiProductCost")
-  
-
-    //-------------------- INDIVIDUAL COST CALCULATOR -------------------------------
-    
-  let calculate_button = document.getElementById("calculateButton")  
-  let total_price = document.getElementsByClassName(".totalPrice")[0]
-    
-  calculate_button.onclick = () => {
-    let total_product_cost_product1 = user_input_product1.value * product_cost_product1.innerHTML
-    multi_product_cost_product1.innerHTML = total_product_cost_product1
-    let total_product_cost_product2 = user_input_product2.value * product_cost_product2.innerHTML
-    multi_product_cost_product2.innerHTML = total_product_cost_product2
-
-    // let cost_of_all_individual_products = document.getElementsByClassName(".multiProductCost")
-
-    // cost_of_all_individual_products.forEach(element => {
-    //   let price_of_all_items = 0
-    //   price_of_all_items += cost_of_all_individual_products[element]
-    //   total_price.innerHTML = price_of_all_items
-    // });
-
-
-function calculatePrices (quantity, price) {
-  
+  for (let i = 0; i < product_container.length; i++) {
+    // en esta función itero sobre el array de los divs con los productos
+    let product_cost = document.getElementsByClassName('productCost')[i]
+    let user_input = document.getElementsByClassName('userInput')[i] 
+    let costOfAllUnits= product_cost.innerHTML * user_input.value
+    let total_product_cost = document.getElementsByClassName('totalProductCost')
+    total_product_cost[i].innerHTML = costOfAllUnits
+    // y ahora hago que sume todo
+    calcTotalPrice += costOfAllUnits
+  }
+    // y que salga en el campo de totalPrice del html
+    let totalPriceDisplay = document.getElementById('totalPrice')
+    totalPriceDisplay.innerHTML = calcTotalPrice
 }
 
-    
-  }
+// voy a hacer que los bototes delete borren cada uno lo suyo
+
+let deleteButtons = document.getElementsByClassName('buttonDelete') // array con los botones
 
 
- 
- //-------------------- TOTAL PRICE CALCULATOR -----------------------------------------
-//  let cost_of_all_individual_products = document.getElementsByClassName(".multiProductCost")
- 
-//  calculate_button.onclick = () => {
-//   cost_of_all_individual_products.forEach(element => {
-//     multi_product_cost.push(cost_of_all_individual_products[element]) 
-//     multi_product_cost_product1.innerHTML
-//   }
+// a continuación la función para que reconozca el botón de delete pulsado y borre todo el producto:
+// 1. saco arriba el array con todos los botones delete
+// 2. hago la función e.currentTarget para que seleccione de todos sólo el que se ha pulsado
+// 3. identifico los parent y childs para poder aplicar el parent.removeChild(child)
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = function (e) { // esto es el e.currentTarget
+    let divButton = deleteButtons[i].parentNode // Me selecciona el div que contiene ese botón (está en un div por el display flex)
+    let div = divButton.parentNode // Me selecciona el div que contiene todo el producto
+    let allTheDivs = div.parentNode // Me selecciona el div que contiene todos los div individuales de cada producto
+    allTheDivs.removeChild(div) //Me borra todo el div child del parent que contiene a todos
+    console.log(div)
+    console.log(allTheDivs)
+  }  
+}
 
-//  }
 
-// console.log(cost_of_all_individual_products)
+let create_button = document.getElementById('createButton')
 
-// }
-// getPriceByProduct(totalProductCost)
+let new_item = document.getElementsByClassName('newItem')[0]
+let new_item_unit_price = document.getElementsByClassName('newItemUnitPrice')[0]
 
-// function updatePriceByProduct(productPrice, index){
+let body = document.getElementsByTagName("body")[0]
+let addNewProdDiv = document.getElementsByClassName('addNewProd')[0]
+let div = document.createElement('div')
+let noClassDiv1 = document.createElement('div')
+let noClassDiv2 = document.createElement('div')
+let noClassDiv3 = document.createElement('div')
+let noClassDiv4 = document.createElement('div')
+let noClassDiv5 = document.createElement('div')
+let spanProductName = document.createElement('span')
+let spanProductCost = document.createElement('span')
+let totalProductCost = document.createElement('span')
+let p1 = document.createElement('p')
+let p2 = document.createElement('p')
+let label = document.createElement('label')
+let input = document.createElement('input')
+let button = document.createElement('button')
 
-// }
+create_button.onclick = () => {
+  
+    body.insertBefore(div, addNewProdDiv)
+    div.className = 'productContainer'
+    div.appendChild(noClassDiv1)
+    noClassDiv1.appendChild(spanProductName)
+    spanProductName.className = 'productName'
+    spanProductName.innerHTML = new_item.value
+    div.appendChild(noClassDiv2)
+    noClassDiv2.appendChild(p1)
+    p1.innerHTML = '$'
+    p1.appendChild(spanProductCost)
+    spanProductCost.className = 'productCost'
+    spanProductCost.innerHTML = new_item_unit_price.value
+    div.appendChild(noClassDiv3)
+    noClassDiv3.appendChild(label)
+    label.innerHTML = 'Qty'
+    noClassDiv3.appendChild(input)
+    div.appendChild(noClassDiv4)
+    noClassDiv4.appendChild(p2)
+    p2.innerHTML = '$'
+    p2.appendChild(totalProductCost)
+    totalProductCost.className = 'totalProductCost'
+    spanProductCost.innerHTML = '0'
+    div.appendChild(noClassDiv5)
+    noClassDiv5.appendChild(button)
+    button.className= 'button buttonDelete'
+    button.innerHTML = 'Delete'
 
-// function getTotalPrice() {
+}
 
-// }
 
-// function createQuantityInput(){
 
-// }
-
-// function createDeleteButton(){
-
-// }
-
-// function createQuantityNode(){
-
-// }
-
-// function createItemNode(dataType, itemData){
-
-// }
-
-// function createNewItemRow(itemName, itemUnitPrice){
-
-// }
-
-// function createNewItem(){
-
-// }
-
-// window.onload = function(){
-//   var calculatePriceButton = document.getElementById('calc-prices-button');
-//   var createItemButton = document.getElementById('new-item-create');
-//   var deleteButtons = document.getElementsByClassName('btn-delete');
-
-//   calculatePriceButton.onclick = getTotalPrice;
-//   createItemButton.onclick = createNewItem;
-
-//   for(var i = 0; i<deleteButtons.length ; i++){
-//     deleteButtons[i].onclick = deleteItem;
-//   }
-// }
